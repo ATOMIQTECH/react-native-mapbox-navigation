81c916815bbccc2d283baa183d1936c1e53ebffd
irere123 <irere2020@outlook.com>
2026-03-04 09:12:29 +0200

    feat: example app configurations

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,35 @@
-import Navigation from './Navigation'
+import { }from '@atomiqlab/react-native-mapbox-navigation';
+import { ScrollView, Text } from 'react-native';
 
 export default function App() {
-  return <Navigation />
+  return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.header}>Module API Example</Text>
+      </ScrollView>
+  );
 }
+
+const styles = {
+  header: {
+    fontSize: 30,
+    margin: 20,
+  },
+  groupHeader: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  group: {
+    margin: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+  },
+  view: {
+    flex: 1,
+    height: 200,
+  },
+};
